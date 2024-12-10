0b3599d4f86000bb71ab0534350171d59c2888bd
joker <exemple@mail.com>
2024-12-10 22:39:27 +0300

    The ,main app is ready, pages index and about

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from operator import index
+from django.urls import include, path
+from main import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.index, name='index' ),
+    path('about/', views.about, name='about' ),
+]
--- main/views.py
+++ main/views.py
@@ -4,12 +4,20 @@ from django.http import HttpResponse
 
 def index(request) -> HttpResponse:
     context: dict = {
-        'title': 'Home', 
-        'content': 'Главная страница - HOME'
+        'title': 'Home - Главная ', 
+        'content':'Магазин мебели HOME'
+        
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request) -> HttpResponse:
-    return render(request, 'main/about.html')
+    context: dict = {
+        'title': 'Home - О нас ', 
+        'content':'О нас',
+        'text_on_page': "Текст какой то"
+        
+    }
+
+    return render(request, 'main/about.html', context)
