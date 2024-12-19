4e437deea643b90947588d63f2c09e29c491fe2e
joker <exemple@mail.com>
2024-12-19 19:48:24 +0300

    Added goods app, connected catalog & product html

---
+++ goods/urls.py
@@ -0,0 +1,10 @@
+from operator import index
+from django.urls import include, path
+from goods import views
+
+app_name = 'goods'
+
+urlpatterns = [
+    path('', views.catalog, name='index' ),
+    path('product/', views.product, name='product' ),
+]
