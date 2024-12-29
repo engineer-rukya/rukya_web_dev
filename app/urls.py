9937a7484058b853a1dbed7fa7586d1dce9b59ac
joker <exemple@mail.com>
2024-12-29 19:30:13 +0300

    Registered simple tag for catigories

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
 from app.settings import DEBUG
+from django.conf.urls.static import static
+
+from app import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +28,9 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
-        path("__debug__/", include("debug_toolbar.urls")),           
+        path("__debug__/", include("debug_toolbar.urls")),    
+
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
