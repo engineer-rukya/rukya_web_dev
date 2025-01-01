431849e3936752cb4dadffcc45965b13b18c8f88
joker <exemple@mail.com>
2025-01-01 18:17:34 +0300

    PostgreSQL data base connacted & full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,7 +1,6 @@
 from goods.models import Products
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.db.models import Q
-
 
 
 
@@ -9,14 +8,22 @@ def q_search(query):
 
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    
+    # .objects.annotate(search=SearchVector("name", "description")).filter(search=query)
 
-    keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # q_objects = Q()
 
-    return Products.objects.filter(q_objects)
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Products.objects.filter(q_objects)
 
