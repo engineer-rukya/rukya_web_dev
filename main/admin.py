1d2ce99f26fb17093b4b9c9fc33412f745bccf3f
joker <exemple@mail.com>
2025-01-03 16:38:03 +0300

    Login form and controller are ready

--- main/admin.py
+++ main/admin.py
@@ -1,20 +1,2 @@
 from django.contrib import admin
-from django.contrib.postgres.fields import JSONField
-import json
-from goods.models import Categories, Products
-
-# admin.site.register(Categories)
-# admin.site.register(Products)
-ensure_ascii=False
-encoding='utf-8'
-
-@admin.register(Categories)
-class CategoriesAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
-
-
-@admin.register(Products)
-class ProductsAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
-
 
