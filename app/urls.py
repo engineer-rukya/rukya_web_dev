4bf2fcb21443c3a7506951335d57eef215cc993d
joker <exemple@mail.com>
2025-07-02 19:25:38 +0300

    Create order is ready

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from debug_toolbar.toolbar import debug_toolbar_urls
+# from debug_toolbar.toolbar import debug_toolbar_urls
 from app.settings import DEBUG
 from django.conf.urls.static import static
 
