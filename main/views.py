52b28a6e76ddda9cdf1959a38ffcecc10275e964
joker <exemple@mail.com>
2025-08-07 17:59:08 +0300

    zabila pro commit

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
+from pydoc import text
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-from goods.models import Categories
+import csv
+import os
+from django.conf import settings
 
 
 
@@ -12,7 +14,7 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Home - Главная'
-        context['content'] = "Магазин мебели HOME"
+        context['content'] = "Услуги разработчика"
         return context
 
 
@@ -21,11 +23,29 @@ class AboutView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Home - О нас'
-        context['content'] = "О нас"
-        context['text_on_page'] = "Текст о том почему этот магазин такой классный, и какой хороший товар."
+        
+        file_path = os.path.join(settings.BASE_DIR,'main/about.csv')
+        data = {}
+        
+        try:
+            with open(file_path, newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    data[row['section']] = row['text']
+        except FileNotFoundError:
+            data = {
+                'title': 'Про нас',
+                'content': 'Привет!',
+                'text_on_page': 'Текст временно недоступен.'
+            }
+        
+        context['title'] = data.get('title', 'Про нас')
+        context['content'] = data.get('content', 'Про нас')
+        context['text_on_page'] = data.get('text_on_page', '')
+        
         return context
-    
+
+
 
 # def index(request):
 
