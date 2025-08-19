1bed3052e9acb25e9ddf91631b14f5365a070122
joker <exemple@mail.com>
2025-08-19 14:44:44 +0300

    Fix catalog AJAX empty results for Django 5

--- goods/views.py
+++ goods/views.py
@@ -4,22 +4,29 @@ from django.views.generic import DetailView, ListView
 from goods.models import Products
 from goods.utils import q_search
 
+from django.http import Http404
+from django.views.generic import DetailView, ListView
+
+from goods.models import Products
+from goods.utils import q_search
+
+
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 class CatalogView(ListView):
     model = Products
-    # queryset = Products.objects.all().order_by("-id")
     template_name = "goods/catalog.html"
     context_object_name = "goods"
     paginate_by = 3
-    allow_empty = False
-    # чтоб удобно передать в методы
+    allow_empty = True
     slug_url_kwarg = "category_slug"
 
     def get_queryset(self):
         category_slug = self.kwargs.get(self.slug_url_kwarg)
+        query = self.request.GET.get("q")
         on_sale = self.request.GET.get("on_sale")
         order_by = self.request.GET.get("order_by")
-        query = self.request.GET.get("q")
 
         if category_slug == "all":
             goods = super().get_queryset()
@@ -27,8 +34,6 @@ class CatalogView(ListView):
             goods = q_search(query)
         else:
             goods = super().get_queryset().filter(category__slug=category_slug)
-            if not goods.exists():
-                raise Http404()
 
         if on_sale:
             goods = goods.filter(discount__gt=0)
@@ -37,13 +42,36 @@ class CatalogView(ListView):
             goods = goods.order_by(order_by)
 
         return goods
-    
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            html = render_to_string(
+                self.template_name,  # вся страница
+                context,
+                request=self.request
+            )
+            if not context["goods"]:
+                html = render_to_string(
+                    "goods/catalog_empty.html",  # шаблон для пустого списка
+                    context,
+                    request=self.request
+                )
+            return JsonResponse({"html": html})
+        else:
+            return super().render_to_response(context, **response_kwargs)
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Home - Каталог"
         context["slug_url"] = self.kwargs.get(self.slug_url_kwarg)
+        # Добавляем опции сортировки для шаблона
+        context["order_options"] = [
+            ("default", "По умолчанию"),
+            ("price", "От дешевых к дорогим"),
+            ("-price", "От дорогих к дешевым"),
+        ]
         return context
-
 
 class ProductView(DetailView):
 
@@ -62,6 +90,66 @@ class ProductView(DetailView):
         context = super().get_context_data(**kwargs)
         context["title"] = self.object.name
         return context
+
+
+
+# class CatalogView(ListView):
+#     model = Products
+#     # queryset = Products.objects.all().order_by("-id")
+#     template_name = "goods/catalog.html"
+#     context_object_name = "goods"
+#     paginate_by = 3
+#     allow_empty = False
+#     # чтоб удобно передать в методы
+#     slug_url_kwarg = "category_slug"
+
+#     def get_queryset(self):
+#         category_slug = self.kwargs.get(self.slug_url_kwarg)
+#         on_sale = self.request.GET.get("on_sale")
+#         order_by = self.request.GET.get("order_by")
+#         query = self.request.GET.get("q")
+
+#         if category_slug == "all":
+#             goods = super().get_queryset()
+#         elif query:
+#             goods = q_search(query)
+#         else:
+#             goods = super().get_queryset().filter(category__slug=category_slug)
+#             if not goods.exists():
+#                 raise Http404()
+
+#         if on_sale:
+#             goods = goods.filter(discount__gt=0)
+
+#         if order_by and order_by != "default":
+#             goods = goods.order_by(order_by)
+
+#         return goods
+    
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["title"] = "Home - Каталог"
+#         context["slug_url"] = self.kwargs.get(self.slug_url_kwarg)
+#         return context
+
+
+# class ProductView(DetailView):
+
+#     # model = Products
+#     # slug_field = "slug"
+#     template_name = "goods/product.html"
+#     slug_url_kwarg = "product_slug"
+#     context_object_name = "product"
+
+#     def get_object(self, queryset=None):
+#         product = Products.objects.get(slug=self.kwargs.get(self.slug_url_kwarg))
+#         return product
+    
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["title"] = self.object.name
+#         return context
 
 
 # def catalog(request, category_slug=None):
